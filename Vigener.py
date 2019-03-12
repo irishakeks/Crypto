b1a47355d926213959fb841a62825f8faabe420d
irishakeks <irishakeks@mail.ru>
2019-03-12 21:58:56 +0700

    Добавлена расшифровка Вижинера

--- Vigener.py
+++ Vigener.py
@@ -24,13 +24,10 @@ class Cipher(QtWidgets.QDialog, VigenerDesign.Ui_AtbashForm):
 
         iter = 0
         for letter in input_text:
-            print('letter: ', letter)
             flag = False
             if iter > len(key) - 1:
-                print("iter % len(key): ", iter % len(key))
                 key_n = self.take_key(key[iter % len(key)])
             else:
-                print("iter: ", iter)
                 key_n = self.take_key(key[iter])
             count = 0
             for element in self.rusABC:
@@ -58,9 +55,7 @@ class Cipher(QtWidgets.QDialog, VigenerDesign.Ui_AtbashForm):
 
             for element in self.engABC:
                 count += 1
-                print("in eng ABC | count: ", count)
                 if letter == element:
-                    print('in if letter == element')
                     flag = True
                     index = self.engABC.index(element)
                     key_n = int(key_n)
@@ -111,7 +106,73 @@ class Cipher(QtWidgets.QDialog, VigenerDesign.Ui_AtbashForm):
                 return key
 
     def decrypt(self):
-        pass
+        input_text = self.textEdit.toPlainText()
+
+        key = self.get_key()
+        result = ""
+
+        iter = 0
+        for letter in input_text:
+            flag = False
+            if iter > len(key) - 1:
+                key_n = self.take_key(key[iter % len(key)])
+            else:
+                key_n = self.take_key(key[iter])
+            count = 0
+            for element in self.rusABC:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC.index(element)
+                    key_n = int(key_n)
+                    a = index - key_n
+                    if a > 32:
+                        a = a % 33
+                    iter = iter + 1
+                    result += self.rusABC[a]
+                    break
+            for element in self.rusABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = index - key_n
+                    if a > 32:
+                        a = a % 33
+                    iter = iter + 1
+                    result += self.rusABC_lower[a]
+                    break
+
+            for element in self.engABC:
+                count += 1
+                if letter == element:
+
+                    flag = True
+                    index = self.engABC.index(element)
+                    key_n = int(key_n)
+                    a = index - key_n
+                    if a > 25:
+                        a = a % 26
+                    iter = iter + 1
+                    result += self.engABC[a]
+                    break
+
+            for element in self.engABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.engABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = index - key_n
+                    if a > 25:
+                        a = a % 26
+                    iter = iter + 1
+                    result += self.engABC_lower[a]
+                    break
+
+            if flag == False:
+                result += letter
+
+
+        self.textBrowser.setPlainText("".join(x for x in result))
 
 
 
