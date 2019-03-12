50150ebee9761421ebe22570125a03042e5030d6
irishakeks <irishakeks@mail.ru>
2019-03-12 17:40:04 +0700

    Все до Гронсфельда включительно

--- Gronsefald.py
+++ Gronsefald.py
@@ -17,8 +17,132 @@ class Cipher(QtWidgets.QDialog, GronsefaldDesign.Ui_AtbashForm):
 
 
     def encrypt(self):
-        pass
+        input_text = self.textEdit.toPlainText()
+
+        key = self.get_key()
+        result = ""
+
+        iter = 0
+        for letter in input_text:
+            flag = False
+            if iter > len(key) - 1:
+                key_n = key[iter % len(key)]
+            else:
+                key_n = key[iter]
+            for element in self.rusABC:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC.index(element)
+                    key_n = int(key_n)
+                    a = self.rusABC[index + key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+            for element in self.rusABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = self.rusABC_lower[index + key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+
+            for element in self.engABC:
+                if letter == element:
+                    flag = True
+                    index = self.engABC.index(element)
+                    key_n = int(key_n)
+                    a = self.engABC[index + key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+            for element in self.engABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.engABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = self.engABC_lower[index + key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+
+            if flag == False:
+                result += letter
+
+
+        self.textBrowser.setPlainText("".join(x for x in result))
+
+
+
 
 
     def decrypt(self):
-        pass
+        input_text = self.textEdit.toPlainText()
+
+        key = self.get_key()
+        result = ""
+
+        iter = 0
+        for letter in input_text:
+            flag = False
+            if iter > len(key) - 1:
+                key_n = key[iter % len(key)]
+            else:
+                key_n = key[iter]
+            for element in self.rusABC:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC.index(element)
+                    key_n = int(key_n)
+                    a = self.rusABC[index - key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+            for element in self.rusABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.rusABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = self.rusABC_lower[index - key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+
+            for element in self.engABC:
+                if letter == element:
+                    flag = True
+                    index = self.engABC.index(element)
+                    key_n = int(key_n)
+                    a = self.engABC[index - key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+            for element in self.engABC_lower:
+                if letter == element:
+                    flag = True
+                    index = self.engABC_lower.index(element)
+                    key_n = int(key_n)
+                    a = self.engABC_lower[index - key_n]
+                    iter = iter + 1
+                    result += a
+                    continue
+
+            if flag == False:
+                result += letter
+
+
+        self.textBrowser.setPlainText("".join(x for x in result))
+
+
+
+
+    def get_key(self):
+        key = self.keyEdit.text()
+        try:
+            if int(key) > 0:
+                return key
+            else:
+                return self.show_msg()
+        except:
+            return self.show_msg()
